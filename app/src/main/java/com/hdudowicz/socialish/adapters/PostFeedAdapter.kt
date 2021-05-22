df2f4c01b3541f0e98288f41ae6ab5d0b5095785
Hubert <hdudowicz@gmail.com>
2021-05-22 14:12:20 +0100

    Comments

--- app/src/main/java/com/hdudowicz/socialish/adapters/PostFeedAdapter.kt
+++ app/src/main/java/com/hdudowicz/socialish/adapters/PostFeedAdapter.kt
@@ -19,7 +19,6 @@ import com.hdudowicz.socialish.data.source.PostRepository
 import com.hdudowicz.socialish.databinding.PostListItemBinding
 import com.hdudowicz.socialish.util.ImageUtil
 import com.perfomer.blitz.setTimeAgo
-import java.util.*
 
 /**
  * Adapter class for populating a RecyclerView with Post objects
@@ -55,17 +54,73 @@ class PostFeedAdapter(): ListAdapter<Post, PostFeedAdapter.PostViewHolder>(PostI
         // Setting post image visibility to gone by default, visibility is changed for image posts later
         viewBinding.postImage.visibility = View.GONE
 
+        // Load image for this post item
+        loadPostImage(post, viewBinding)
+
+        // If post body is empty then hide post body TextView
+        if (post.body.isBlank()){
+            viewBinding.postBody.visibility = View.GONE
+        } else {
+            viewBinding.postBody.visibility = View.VISIBLE
+        }
+
+        // Listener for share button using android Sharesheet to share post text to other apps
+        viewBinding.sharePost.setOnClickListener {
+            // Creating share text
+            val shareText = "Socialish Post - \nTitle: ${post.title} \nBody: ${post.body}"
+            // Creating intent for sharing text data with other apps, includes a title and text
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TITLE, post.title)
+                putExtra(Intent.EXTRA_TEXT, shareText)
+                type = "text/plain"
+            }
+            // Opening Android Sharesheet app chooser
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            context.startActivity(shareIntent)
+        }
+
+        // Checking if this post is saved locally and loading the appropriate save button icon
+        val isPostSaved = postRepository.isPostSaved(context, post)
+        loadSaveIconInView(viewBinding.savePost, isPostSaved)
+
+        // Clicking the save post button checks if a post is saved locally, either saves or deletes it then updates the save button icon
+        viewBinding.savePost.setOnClickListener {
+            var savedState = postRepository.isPostSaved(context, post)
+            if (savedState){
+                postRepository.deleteLocalPost(context, post)
+                savedState = false
+            } else {
+                postRepository.savePostLocally(context, post)
+                savedState = true
+            }
+
+            // Loading new save button icon into view
+            loadSaveIconInView(viewBinding.savePost, savedState)
+        }
+    }
+
+    /**
+     * If the passed Post object is an image post, it loads the image into the passed post item's ImageView.
+     * Checks if the post is in local storage or the internet and loads the image appropriately.
+     *
+     * @param post Post object to load an image for
+     * @param viewBinding View binding class for the post list item corresponding to the passed post
+     */
+    private fun loadPostImage(post: Post, viewBinding: PostListItemBinding){
+        val context = viewBinding.root.context
+        // If the current post is an image post then show ImageView and initialise with image
         if (post.isImagePost){
             viewBinding.postImage.visibility = View.VISIBLE
 
-            // Creating progress drawable to show when Glide is loading a post
+            // Creating circular progress drawable to display in ImageView when Glide is loading a post image
             val progressDrawable = CircularProgressDrawable(context).apply {
                 strokeWidth = 6f
                 centerRadius = 35f
             }
             progressDrawable.start()
 
-            // Setting post image with glide.
+            // Setting post image with glide depending on if this adapter is showing locally stored posts
             if (isLocal){
                 // Get locally saved post images using ImageUtil function to get File object of the post image
                 Glide.with(context)
@@ -75,6 +130,7 @@ class PostFeedAdapter(): ListAdapter<Post, PostFeedAdapter.PostViewHolder>(PostI
                     .placeholder(progressDrawable)
                     .into(viewBinding.postImage)
             } else {
+                // If the post is not local then load the image from a URI on the internet
                 Glide.with(context)
                     .load(post.imageUri)
                     .fitCenter()
@@ -86,47 +142,14 @@ class PostFeedAdapter(): ListAdapter<Post, PostFeedAdapter.PostViewHolder>(PostI
             // Clearing glide load to prevent images being loaded in the wrong post due to recycling of views
             Glide.with(context).clear(viewBinding.postImage)
         }
-
-
-        // If post body is empty then hide it's body TextView
-        if (post.body.isBlank()){
-            viewBinding.postBody.visibility = View.GONE
-        } else {
-            viewBinding.postBody.visibility = View.VISIBLE
-        }
-
-        // Listener for share button using android Sharesheet to share post text to other apps
-        viewBinding.sharePost.setOnClickListener {
-            // Creating share text
-            val shareText = "Socialish Post - \nTitle: ${post.title} \nBody: ${post.body}"
-            val sendIntent: Intent = Intent().apply {
-                action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TITLE, post.title)
-                putExtra(Intent.EXTRA_TEXT, shareText)
-                type = "text/plain"
-            }
-            val shareIntent = Intent.createChooser(sendIntent, null)
-            context.startActivity(shareIntent)
-        }
-
-        val isPostSaved = postRepository.isPostSaved(context, post)
-        loadSaveIconInView(viewBinding.savePost, isPostSaved)
-
-        // When clicking save post button it checks if a post is saved locally and either saves or deletes it based on that then updates the save button icon
-        viewBinding.savePost.setOnClickListener {
-            var savedState = postRepository.isPostSaved(context, post)
-            if (savedState){
-                postRepository.deleteLocalPost(context, post)
-                savedState = false
-            } else {
-                postRepository.savePostLocally(context, post)
-                savedState = true
-            }
-
-            loadSaveIconInView(viewBinding.savePost, savedState)
-        }
     }
 
+    /**
+     * Loads the appropriate save button icon into passed ImageView depending on if a post is saved.
+     *
+     * @param imageView view to change the icon of
+     * @param saved saved state of the current post item view
+     */
     private fun loadSaveIconInView(imageView: ImageView, saved: Boolean){
         if (saved){
             Glide.with(imageView.context)
@@ -145,7 +168,14 @@ class PostFeedAdapter(): ListAdapter<Post, PostFeedAdapter.PostViewHolder>(PostI
     }
 
 
-    // Using a ViewHolder allows views to be cached instead of recreating them
+    /**
+     * ViewHolder class containing post card view click listener and item binding class.
+     * It allows views to be cached instead of recreating them
+     *
+     * @property binding
+     *
+     * @constructor Create list item ViewHolder and initialise expand post card on click
+     */
     class PostViewHolder(val binding: PostListItemBinding): RecyclerView.ViewHolder(binding.root) {
         var bodyCollapsed: Boolean = true
         init {
@@ -153,7 +183,7 @@ class PostFeedAdapter(): ListAdapter<Post, PostFeedAdapter.PostViewHolder>(PostI
             binding.cardView.setOnClickListener {
                 bodyCollapsed = !bodyCollapsed
                 TransitionManager.beginDelayedTransition(binding.cardView)
-                // Change max lines showing depending on if body is collapsed
+                // Change max lines showing depending on if post body is collapsed
                 if (bodyCollapsed){
                     binding.postBody.maxLines = 1
                 } else {
@@ -164,7 +194,11 @@ class PostFeedAdapter(): ListAdapter<Post, PostFeedAdapter.PostViewHolder>(PostI
     }
 }
 
-// Callback class for calculating differences between post objects
+/**
+ * Callback class for calculating differences between post objects
+ *
+ * @constructor Create DiffUtil.ItemCallback class for comparing Post objects
+ */
 class PostItemDiffCallback: DiffUtil.ItemCallback<Post>(){
     override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
         return oldItem.title == newItem.title && oldItem.body == newItem.body && oldItem.imageUri == newItem.imageUri
@@ -173,5 +207,4 @@ class PostItemDiffCallback: DiffUtil.ItemCallback<Post>(){
     override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
         return newItem.postId == oldItem.postId
     }
-
 }
